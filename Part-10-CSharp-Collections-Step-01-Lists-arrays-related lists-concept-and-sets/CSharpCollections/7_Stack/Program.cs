2b75c3932cf692a40e04cd9ab129d90dfa5fb986
William Rezende <williamrezendee@gmail.com>
2021-11-03 22:16:11 -0300

    Ending the CSharp Collections - Part 01

---
+++ Part-10-CSharp-Collections-Step-01-Lists-arrays-related lists-concept-and-sets/CSharpCollections/7_Stack/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var _browser = new Browser();
+            _browser.BrowseFor("www.google.com.br");
+            _browser.BrowseFor("www.caelum.com.br");
+            _browser.BrowseFor("www.alura.com.br");
+
+            _browser.GoBackToEarlierPage();
+            _browser.GoBackToEarlierPage();
+            _browser.GoBackToEarlierPage();
+            _browser.GoBackToEarlierPage();
+
+            _browser.GoToNextPage();
+            _browser.GoToNextPage();
+            _browser.GoToNextPage();
+
+        }
+    }
+
+    internal class Browser
+    {
+        private readonly Stack<string> _earlierHistory = new Stack<string>();
+        private readonly Stack<string> _nextHistory = new Stack<string>();
+
+        private string _currentPage = "Empty";
+        public Browser()
+        {
+            Console.WriteLine("Current Page: "+_currentPage);
+        }
+
+        internal void BrowseFor(string url)
+        {
+            _earlierHistory.Push(_currentPage);
+            _currentPage = url;
+            Console.WriteLine("Current Page: "+_currentPage);
+        }
+
+        internal void GoBackToEarlierPage()
+        {
+            if (_earlierHistory.Any())
+            {
+                _nextHistory.Push(_currentPage);
+                _currentPage = _earlierHistory.Pop();
+                Console.WriteLine("Current Page: "+_currentPage);
+            }
+        }
+
+        internal void GoToNextPage()
+        {
+            if (_nextHistory.Any())
+            {
+                _earlierHistory.Push(_currentPage);
+                _currentPage = _nextHistory.Pop();
+                Console.WriteLine("Current Page: "+_currentPage);
+            }
+        }
+    }
+}
---
+++ Part-10-CSharp-Collections-Step-01-Lists-arrays-related lists-concept-and-sets/CSharpCollections/8_Queue/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_Queue
+{
+    class Program
+    {
+        static Queue<string> _tolling = new Queue<string>();
+
+        static void Main(string[] args)
+        {
+            Enqueue("Van");
+            Enqueue("Kombi");
+            Enqueue("Guincho");
+            Enqueue("Pickup");
+            ShowQueue();
+            Dequeue();
+            ShowQueue();
+
+            Console.WriteLine();
+        }
+
+        private static void Enqueue(string vehicle)
+        {
+            Console.WriteLine($"Joined the queue: {vehicle}");
+            _tolling.Enqueue(vehicle);
+        }
+
+        private static void Dequeue()
+        {
+            if (_tolling.Any())
+            {
+                string vehicle = _tolling.Dequeue();
+                Console.WriteLine($"Leave the queue: {vehicle}");
+            }
+        }
+
+        private static void ShowQueue()
+        {
+            Console.Write("Queue: ");
+            foreach (var v in _tolling)
+            {
+                Console.Write($"{v} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+            
